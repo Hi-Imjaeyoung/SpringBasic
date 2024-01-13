a89aee760da70b2bee3328be49577fd63a400222
Hi-Imjaeyoung <imjy2757@naver.com>
2024-01-13 17:47:01 +0900

    add Memeber variable

--- src/main/java/com/encore/basic/domain/Member.java
+++ src/main/java/com/encore/basic/domain/Member.java
@@ -1,4 +1,17 @@
 package com.encore.basic.domain;
 
 public class Member {
+    private String name;
+    private String id;
+    private String pwd;
+
+    Member(MemberDTO memberDTO){
+        this.name = memberDTO.getName();
+        this.id = memberDTO.getId();
+        this.pwd = memberDTO.getPwd();
+    }
+
+    public String toString(){
+        return "name : "+ name +" id : "+id+" pwd : "+pwd;
+    }
 }
