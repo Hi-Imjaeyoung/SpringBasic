e9d3820dfa29e887deb5f490ee7c2d6a1b08f152
Hi-Imjaeyoung <imjy2757@naver.com>
2024-01-15 17:45:37 +0900

    add MemberResponseDto.java

--- src/main/java/com/encore/basic/domain/MemberResponseDto.java
+++ src/main/java/com/encore/basic/domain/MemberResponseDto.java
@@ -3,10 +3,14 @@ package com.encore.basic.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 public class MemberResponseDto {
+    private int id;
     private String name;
     private String email;
     private String pwd;
+    private LocalDateTime createTime;
 }
