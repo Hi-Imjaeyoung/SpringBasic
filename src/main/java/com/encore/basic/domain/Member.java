9a6fdd87f977830f3977957e0301498dcc116997
Hi-Imjaeyoung <imjy2757@naver.com>
2024-01-16 17:45:16 +0900

    fix Member

--- src/main/java/com/encore/basic/domain/Member.java
+++ src/main/java/com/encore/basic/domain/Member.java
@@ -1,10 +1,8 @@
 package com.encore.basic.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
@@ -12,7 +10,7 @@ import java.time.LocalDateTime;
 // setter를 넣지 않는다.
 // -> data를 받는 용도의 DTO클래스를 생성 =
 
-@AllArgsConstructor
+//@AllArgsConstructor
 // 모든 매개변수를 넣은 생성자
 
 //@NoArgsConstructor
@@ -20,10 +18,37 @@ import java.time.LocalDateTime;
 
 //@RequiredArgsConstructor
 //// 필수값 설정 후, 생성자의 매개변수 설정
+
+@Entity
+// Entity를 통해 mariadb의 테이블 및 컬럼을 자동 생성 대상이된다.
+// 조회 할때 리플렉션을 통해 사용
+// 클래스명 > 테이블명, 변수명 컬럼명
+// 만약 생성자가 존재하면 기본생성자가 필요
+@NoArgsConstructor
 public class Member {
+    @Setter
+    //이건 어쩔수 없다. 메모리 DB 때문
+    @Id
+    //identity = auto_increment 설정 , auto = jpa가 자동으로 적절한 키 생성 전략
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    // String = varchar로 변환
     private String name;
+
+    @Column(nullable = false,length = 50)
     private String email;
+
     private String pwd;
+
+    @Setter
+    @Column(name = "created_time") // name 옵션을 통해 DB 컬럼명 별도로 지정가능
     private LocalDateTime createTime;
+
+    public Member(String name, String email,String pwd){
+        this.name =name;
+        this.email =email;
+        this.pwd = pwd;
+        this.createTime = LocalDateTime.now();
+    }
 }
