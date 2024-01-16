b5b263c8b0f3e53a88d60fb133d2e3c73a397a94
Hi-Imjaeyoung <imjy2757@naver.com>
2024-01-16 17:46:35 +0900

    MemberService add

--- src/main/java/com/encore/basic/service/MemberService.java
+++ src/main/java/com/encore/basic/service/MemberService.java
@@ -3,35 +3,35 @@ package com.encore.basic.service;
 import com.encore.basic.domain.Member;
 import com.encore.basic.domain.MemberRequestDto;
 import com.encore.basic.domain.MemberResponseDto;
-import com.encore.basic.repository.MemberRepo;
-import com.encore.basic.repository.MemoryRepo;
+import com.encore.basic.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class MemberService {
 
     private final MemberRepo memberRepo;
-    @Autowired
-    public MemberService(MemoryRepo memoryRepo){
-        this.memberRepo = memoryRepo;
-    }
 
-    static int totalId = 0;
+//    // 실습을 위해 구현체로 선언 MemberRepo은 기능이 제한적으로 구현
+//    private final SpringDataMemberRepository memberRepo;
+    @Autowired
+    public MemberService(MybatisMemberRepository memberRepo){
+        this.memberRepo = memberRepo;
+    }
 
     // 일반적으로 서비스 단에서 DTO 객체를 받아서 entity로 조립
     public void memberCreate(MemberRequestDto memberDto){
-        totalId+=1;
-        LocalDateTime localDateTime = LocalDateTime.now();
-        Member member = new Member(totalId,memberDto.getName(),memberDto.getEmail(),memberDto.getPwd(),localDateTime);
-        memberRepo.memberCreate(member);
+        Member member = new Member(memberDto.getName(),memberDto.getEmail(),memberDto.getPwd());
+        memberRepo.save(member);
     }
     public List<MemberResponseDto> members(){
-       List<Member> members = memberRepo.members();
+       List<Member> members = memberRepo.findAll();
         // 여러 패턴이 존재
         // setter , 생성자 , builder
        List<MemberResponseDto> memberResponseDtos = members
@@ -40,7 +40,13 @@ public class MemberService {
                                                    .collect(Collectors.toList());
        return memberResponseDtos;
     }
-    public Member memberFind(int id){
-       return memberRepo.memberFind(id);
+
+    // 이거 ResponseDTO로 리턴해라~
+    // 무조건 DTO를 써라
+                                                // uncheck이긴 하지만  명시해주는 것이 좋다
+    public MemberResponseDto memberFind(int id) throws NoSuchElementException{
+        Member member = memberRepo.findById(id).orElseThrow(NoSuchElementException::new);
+        MemberResponseDto memberResponseDto = null;
+        return new MemberResponseDto(member.getId(), member.getName(), member.getEmail(), member.getPwd(), member.getCreateTime());
     }
 }
